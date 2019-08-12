9cfec0f2902213fa2412b27543ed6e6df06ae86c
nidhi2611 <nidhi.gupta_cs18@gla.ac.in>
2019-08-12 15:09:52 +0530

    Main.java: add statement for printing hello world

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,8 @@
 package main;
 
 public class Main {
-    public static void main(String args[])
+    public static void main(String args[]) {
+        System.out.println("Hello World!");
+    }
 
 }
