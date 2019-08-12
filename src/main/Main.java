cd30a7fef489f3a821b0b29a697411b4d43b5e3f
nidhi2611 <nidhi.gupta_cs18@gla.ac.in>
2019-08-12 15:40:49 +0530

    Main.java: inputted using console

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,9 +4,12 @@
  */
 package main;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String args[]) {
-        int number = 4;
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
         for (int i = 1; i < 11; i++) {
             System.out.println(number + "x" + i + "=" + (number * i));
 
